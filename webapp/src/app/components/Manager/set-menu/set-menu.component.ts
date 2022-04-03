51ec6e0866ba898e3ccb31183d07856ce1a2a4b6
Davide Magli <davide.magli99@gmail.com>
2022-04-03 20:18:55 +0200

    done some authorization logic and components

--- webapp/src/app/components/Manager/set-menu/set-menu.component.ts
+++ webapp/src/app/components/Manager/set-menu/set-menu.component.ts
@@ -13,8 +13,16 @@ import {v4 as uuid} from 'uuid';
 export class SetMenuComponent extends SubscriberContextComponent implements OnInit {
 
   currentMenu: MenuElement[] = [];
-
+  editingElementId: string | null = null;
   toInsert = this.fb.group(
+    {
+      name: this.fb.control(null, [Validators.required]),
+      ingredients: this.fb.control(null, [Validators.required]),
+      description: this.fb.control(null, [Validators.required]),
+      price: this.fb.control(null, [Validators.required])
+    }
+  );
+  editing = this.fb.group(
     {
       name: this.fb.control(null, [Validators.required]),
       ingredients: this.fb.control(null, [Validators.required]),
@@ -37,11 +45,31 @@ export class SetMenuComponent extends SubscriberContextComponent implements OnIn
     const newElement = this.toInsert.value as MenuElement;
     newElement.uuid = uuid(); // setting this does nothing backend side it gets overrided
     this.currentMenu.push(newElement);
-    console.log(newElement)
     this.toInsert.reset();
   }
 
   resetElement(): void {
     this.toInsert.reset();
+  }
+
+  remove(menuElement: MenuElement): void {
+    this.currentMenu = this.currentMenu.filter(it => it.uuid !== menuElement.uuid);
+  }
+
+  startEdit(menuElement: MenuElement): void {
+    this.editingElementId = menuElement.uuid;
+    this.editing.setValue(menuElement);
+  }
+
+  endEdit(menuElement: MenuElement): void {
+    this.editingElementId = null;
+    this.currentMenu.filter(it => it.uuid === menuElement.uuid)[0] = this.editing.value;// aggiungere setting ogggetto da editing form
+    this.editing.reset();
+  }
+
+  cancelEdit(menuElement: MenuElement): void {
+    this.editingElementId = null;
+    this.editing.reset();
+
   }
 }
